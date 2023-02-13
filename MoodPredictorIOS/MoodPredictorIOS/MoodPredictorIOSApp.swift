d61bee17d9dbcf469289058f7c62b62f0b70a20f
Brett Mulder <brettmulder@Bretts-MacBook-Pro.local>
2023-02-13 09:49:57 +0100

    Created project

---
+++ MoodPredictorIOS/MoodPredictorIOS/MoodPredictorIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodPredictorIOSApp.swift
+//  MoodPredictorIOS
+//
+//  Created by Brett Mulder on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoodPredictorIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
